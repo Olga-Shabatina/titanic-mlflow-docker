654eaaab8626e90f09158a39932aab9e8ac75377
Olga Shabatina <96422402+Olga-Shabatina@users.noreply.github.com>
2026-02-12 20:35:07 +0100

    train/test split

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
 
 titanic = fetch_openml("titanic", version=1, as_frame=True)
 df = titanic.frame
@@ -14,3 +15,5 @@ df['sex'] = df['sex'].map({'male': 0, 'female': 1})
 X = df[['pclass', 'sex', 'age', 'fare']]
 # target vector
 y = df['survived'].astype(int)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
