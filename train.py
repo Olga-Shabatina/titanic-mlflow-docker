456898f767a175fd79da799158ce4375a5039a9c
Olga Shabatina <96422402+Olga-Shabatina@users.noreply.github.com>
2026-02-12 20:45:44 +0100

    train model

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.datasets import fetch_openml
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 titanic = fetch_openml("titanic", version=1, as_frame=True)
 df = titanic.frame
@@ -17,3 +18,7 @@ X = df[['pclass', 'sex', 'age', 'fare']]
 y = df['survived'].astype(int)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
+# train model
+model = RandomForestClassifier(n_estimators = 100, random_state = 42)
+model.fit(X_train, y_train)
