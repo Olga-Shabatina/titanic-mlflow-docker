28998129269ccf115e7f8b8616f6fc919e9b020c
Olga Shabatina <96422402+Olga-Shabatina@users.noreply.github.com>
2026-02-12 20:16:26 +0100

    preprocess

--- train.py
+++ train.py
@@ -5,3 +5,12 @@ titanic = fetch_openml("titanic", version=1, as_frame=True)
 df = titanic.frame
 
 print(df.head())
+
+# preprocess dataset
+df = df[['pclass', 'sex', 'age', 'fare', 'survived']].dropna()
+df['sex'] = df['sex'].map({'male': 0, 'female': 1})
+
+# features matrix
+X = df[['pclass', 'sex', 'age', 'fare']]
+# target vector
+y = df['survivev'].astype(int)
